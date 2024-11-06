d1cf08a5d6a8e3d00de31e452a6f97e7f772c379
sahanyaL <piyumdi.yunash@gmail.com>
2024-11-06 12:17:04 +0530

    fixed canvas issue (game tips screen, shop and rank screen)

--- Assets/Scripts/UI/Canvas/LoginCanvas.cs
+++ Assets/Scripts/UI/Canvas/LoginCanvas.cs
@@ -191,191 +191,7 @@ namespace Yunash.UI
 
 */
 
-/*
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using Yunash.Game;
-using Yunash.Audio; // Ensure this is included to access your custom AudioType
 
-namespace Yunash.UI
-{
-    public class LoginCanvas : CanvasBase
-    {
-        //
-        public static LoginCanvas Instance { get; private set; }
-        //
-
-        [Header("Elements")]
-        [SerializeField] private GameObject menuPanel;
-        [SerializeField] private GameObject gamePanel;
-        [SerializeField] private GameObject gameOverPanel;
-        [SerializeField] private GameObject levelCompletePanel;
-        [SerializeField] private GameObject SettingsPanel;
-        [SerializeField] private GameObject PausePanel;
-        [SerializeField] private GameObject ShopPanel;
-
-        [SerializeField] private TMP_Text levelText;
-
-        private AudioManager audioManager; // Declare an AudioManager variable
-
-        //
-        private void Awake()
-        {
-            if (Instance == null)
-            {
-                Instance = this;
-                DontDestroyOnLoad(gameObject);
-            }
-            else
-            {
-                Destroy(gameObject);
-            }
-        }
-        //
-
-
-        void Start()
-        {
-            // Initialize the AudioManager
-            audioManager = FindObjectOfType<AudioManager>(); // Find the AudioManager in the scene
-            if (audioManager == null)
-            {
-                Debug.LogError("AudioManager not found in the scene!");
-            }
-
-            // Set initial panel states
-            menuPanel.SetActive(true);
-            gamePanel.SetActive(false);
-            gameOverPanel.SetActive(false);
-            levelCompletePanel.SetActive(false);
-            SettingsPanel.SetActive(false);
-            PausePanel.SetActive(false);
-            ShopPanel.SetActive(false);
-
-            GameManager.onGameStateChanged += GameStateChangedCallBack;
-            StartCoroutine(WaitForRoomManager());
-        }
-
-        IEnumerator WaitForRoomManager()
-        {
-            yield return new WaitUntil(() => RoomManager.instance != null); // Wait until RoomManager instance is ready
-        }
-
-        private void OnDestroy()
-        {
-            GameManager.onGameStateChanged -= GameStateChangedCallBack;
-            //
-            if (Instance == this)
-            {
-                Instance = null;
-            }
-            //
-        }
-
-        private void GameStateChangedCallBack(GameManager.GameState gameState)
-        {
-            if (gameState == GameManager.GameState.GameOver)
-                ShowGameOver();
-            else if (gameState == GameManager.GameState.LevelComplete)
-                ShowLevelComplete();
-        }
-
-        public void PlayButtonPressed()
-        {
-            GameManager.Instance.SetGameState(GameManager.GameState.Game);
-            menuPanel.SetActive(false);
-            gamePanel.SetActive(true);
-        }
-
-        private void ShowLevelComplete()
-        {
-            levelCompletePanel.SetActive(true);
-            audioManager?.PlayAudio(Yunash.Audio.AudioType.LevelComplete); // Play level complete music
-        }
-
-        public void HideLevelComplete()
-        {
-            Debug.Log("Clicked");
-            levelCompletePanel.SetActive(false);
-            audioManager?.PlayAudio(Yunash.Audio.AudioType.IdleBackgroundMusic); // Revert to idle background music
-            gamePanel.SetActive(true);
-            Debug.Log("end");
-        }
-
-        public void ShowGameOver()
-        {
-            gameOverPanel.SetActive(true);
-            Time.timeScale = 0; // Freeze the game
-        }
-
-        public void RetryButtonPressed()
-        {
-            Time.timeScale = 1; // Resume normal game speed
-            //SceneManager.LoadScene(0); // Reload the scene
-            //
-            ResetGameState();
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Reload the current scene
-            //
-        }
-
-        //
-        private void ResetGameState()
-        {
-            // Reset relevant UI panels
-            gamePanel.SetActive(false);
-            gameOverPanel.SetActive(false);
-            levelCompletePanel.SetActive(false);
-            SettingsPanel.SetActive(false);
-            PausePanel.SetActive(false);
-            ShopPanel.SetActive(false);
-
-            // Reset any other states in GameManager if necessary
-            // You might also need to reset persistent data here, e.g., player score, level progress
-        }
-        //
-
-        // Settings and Shop panel handling methods
-        public void ShowSettingsPanel()
-        {
-            SettingsPanel.SetActive(true);
-        }
-
-        public void HideSettingsPanel()
-        {
-            SettingsPanel.SetActive(false);
-        }
-
-        public void ShowPausePanel()
-        {
-            PausePanel.SetActive(true);
-        }
-
-        public void HidePausePanel()
-        {
-            PausePanel.SetActive(false);
-        }
-
-        public void ShowShopPanel()
-        {
-            ShopPanel.SetActive(true);
-        }
-
-        public void HideShopPanel()
-        {
-            ShopPanel.SetActive(false);
-        }
-
-        // Add this method if you prefer the ShowGameOverPanel name
-        public void ShowGameOverPanel()
-        {
-            ShowGameOver(); // Reuse the existing method
-        }
-    }
-}
-*/
 
 
 using System.Collections;
@@ -402,6 +218,9 @@ namespace Yunash.UI
         [SerializeField] private GameObject SettingsPanel;
         [SerializeField] private GameObject PausePanel;
         [SerializeField] private GameObject ShopPanel;
+
+        [SerializeField] private GameObject gameTipsPanel;
+        [SerializeField] private GameObject ranksPanel;
 
         [SerializeField] private TMP_Text levelText;
 
@@ -440,6 +259,10 @@ namespace Yunash.UI
             PausePanel.SetActive(false);
             ShopPanel.SetActive(false);
 
+            gameTipsPanel.SetActive(false); // Hide GameTips panel initially
+            ranksPanel.SetActive(false); // Hide Ranks panel initially
+
+
             GameManager.onGameStateChanged += GameStateChangedCallBack;
             StartCoroutine(WaitForRoomManager());
         }
@@ -474,6 +297,27 @@ namespace Yunash.UI
             menuPanel.SetActive(false);
             gamePanel.SetActive(true);
         }
+
+        public void ShowGameTipsPanel()
+        {
+            gameTipsPanel.SetActive(true);
+        }
+
+        public void HideGameTipsPanel()
+        {
+            gameTipsPanel.SetActive(false);
+        }
+
+        public void ShowRanksPanel()
+        {
+            ranksPanel.SetActive(true);
+        }
+
+        public void HideRanksPanel()
+        {
+            ranksPanel.SetActive(false);
+        }
+
 
         private void ShowLevelComplete()
         {
@@ -566,15 +410,15 @@ namespace Yunash.UI
             Time.timeScale = 1; // Resume the game when hiding the pause panel
         }
 
-        public void ShowShopPanel()
+        public void ShowShopPanell()
         {
             ShopPanel.SetActive(true);
         }
-
         public void HideShopPanel()
         {
             ShopPanel.SetActive(false);
         }
+
 
         // Add this method if you prefer the ShowGameOverPanel name
         public void ShowGameOverPanel()
